4589ed22d005a8e3a142f148b05901daa541424e
BRAHIM-coder <ousmanealibrahim@esp.sn>
2024-04-03 22:57:59 +0000

    second commit

--- function.py
+++ function.py
@@ -30,3 +30,8 @@ def customerRetentionRate():
     conn.commit()
     cur.execute("SELECT (COUNT(DISTINCT order_id_recurrent) / COUNT(DISTINCT order_id)) * 100 AS taux_retour_client FROM    (SELECT   o1.customer_id,            o1.order_id AS order_id_recurrent,MIN(o2.order_id) AS order_id FROM oc_order o1 LEFT JOIN oc_order o2 ON o1.customer_id = o2.customer_id AND o1.order_id < o2.order_id    GROUP BY o1.order_id) AS recurrent_orders")
     return int(cur.fetchone()[0])
+
+def listOfBestSellingProducts():
+    cur.execute("SELECT p.product_id, pd.name, SUM(op.quantity) AS total_quantity FROM oc_order_product op JOIN oc_product p ON op.product_id = p.product_id JOIN oc_product_description pd ON p.product_id = pd.product_id WHERE p.status = '1' GROUP BY p.product_id ORDER BY total_quantity DESC LIMIT 10")
+    resultats = cur.fetchall()
+    
